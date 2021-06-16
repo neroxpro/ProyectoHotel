ea2a7b974028eca54ad79a792eef9cc1981cf09c
Fer <Fer@DESKTOP-MT7OL18>
2021-06-16 18:39:04 -0500

    nuevo login

--- src/Jframes/Login.java
+++ src/Jframes/Login.java
@@ -6,6 +6,7 @@
 package Jframes;
 
 import controlMySql.MySqlConn;
+import java.applet.AudioClip;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 import org.apache.commons.codec.digest.DigestUtils;
@@ -39,9 +40,11 @@ public class Login extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         jPasswordFieldcontra = new javax.swing.JPasswordField();
         jLabel4 = new javax.swing.JLabel();
-        jLabel6 = new javax.swing.JLabel();
         jButtonRegistrar = new javax.swing.JButton();
+        jButtonMUSICA = new javax.swing.JButton();
+        jButtonSTOP = new javax.swing.JButton();
         jLabel3 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -57,27 +60,23 @@ public class Login extends javax.swing.JFrame {
             }
         });
         jPanel1.add(jButtonEntrar);
-        jButtonEntrar.setBounds(230, 470, 73, 23);
+        jButtonEntrar.setBounds(360, 540, 79, 25);
 
         jLabel1.setFont(new java.awt.Font("Arial Black", 0, 12)); // NOI18N
         jLabel1.setText("Usuario:");
         jPanel1.add(jLabel1);
-        jLabel1.setBounds(30, 330, 100, 19);
+        jLabel1.setBounds(170, 400, 100, 19);
         jPanel1.add(jTextFieldUsuario);
-        jTextFieldUsuario.setBounds(140, 330, 231, 20);
+        jTextFieldUsuario.setBounds(260, 400, 231, 22);
 
         jLabel2.setFont(new java.awt.Font("Arial Black", 0, 11)); // NOI18N
         jLabel2.setText("Contraseña:");
         jPanel1.add(jLabel2);
-        jLabel2.setBounds(10, 420, 110, 17);
+        jLabel2.setBounds(150, 490, 110, 17);
         jPanel1.add(jPasswordFieldcontra);
-        jPasswordFieldcontra.setBounds(140, 420, 231, 20);
+        jPasswordFieldcontra.setBounds(260, 490, 231, 22);
         jPanel1.add(jLabel4);
         jLabel4.setBounds(380, 220, 0, 0);
-
-        jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/green box2.png"))); // NOI18N
-        jPanel1.add(jLabel6);
-        jLabel6.setBounds(380, 0, 150, 70);
 
         jButtonRegistrar.setText("REGISTRAR");
         jButtonRegistrar.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -86,21 +85,48 @@ public class Login extends javax.swing.JFrame {
             }
         });
         jPanel1.add(jButtonRegistrar);
-        jButtonRegistrar.setBounds(20, 20, 120, 30);
+        jButtonRegistrar.setBounds(560, 220, 120, 30);
+
+        jButtonMUSICA.setText("MUSICA");
+        jButtonMUSICA.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButtonMUSICAMouseClicked(evt);
+            }
+        });
+        jButtonMUSICA.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonMUSICAActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButtonMUSICA);
+        jButtonMUSICA.setBounds(580, 110, 90, 25);
+
+        jButtonSTOP.setText("STOP");
+        jButtonSTOP.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButtonSTOPMouseClicked(evt);
+            }
+        });
+        jPanel1.add(jButtonSTOP);
+        jButtonSTOP.setBounds(590, 170, 65, 25);
 
         jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/user-login.png"))); // NOI18N
         jPanel1.add(jLabel3);
-        jLabel3.setBounds(0, -10, 500, 510);
+        jLabel3.setBounds(140, 60, 500, 510);
+
+        jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/fondo coco5.png"))); // NOI18N
+        jPanel1.add(jLabel6);
+        jLabel6.setBounds(0, 0, 750, 490);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 500, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 750, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 500, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 568, Short.MAX_VALUE)
         );
 
         pack();
@@ -150,6 +176,24 @@ public class Login extends javax.swing.JFrame {
         this.dispose();
     }//GEN-LAST:event_jButtonRegistrarMouseClicked
 
+    private void jButtonMUSICAActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonMUSICAActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButtonMUSICAActionPerformed
+
+    private void jButtonMUSICAMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButtonMUSICAMouseClicked
+        // TODO add your handling code here:
+        AudioClip piano2;
+        piano2=java.applet.Applet.newAudioClip(getClass().getResource("/Jframes/piano2.wav"));
+        piano2.play();
+    }//GEN-LAST:event_jButtonMUSICAMouseClicked
+
+    private void jButtonSTOPMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButtonSTOPMouseClicked
+        // TODO add your handling code here:
+        AudioClip piano2;
+        piano2=java.applet.Applet.newAudioClip(getClass().getResource("/Jframes/piano2.wav"));
+        piano2.stop();
+    }//GEN-LAST:event_jButtonSTOPMouseClicked
+
     /**
      * @param args the command line arguments
      */
@@ -187,7 +231,9 @@ public class Login extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButtonEntrar;
+    private javax.swing.JButton jButtonMUSICA;
     private javax.swing.JButton jButtonRegistrar;
+    private javax.swing.JButton jButtonSTOP;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
